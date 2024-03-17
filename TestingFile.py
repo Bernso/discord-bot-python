a089c2eb4d6fc67773d123344fab69ceafbc64e7
benwsheridan <ben.w.sheridan@gmail.com>
2024-03-17 16:18:14 +0000

    changed the status message of the bot.

--- TestingFile.py
+++ TestingFile.py
@@ -12,7 +12,7 @@ import sqlite3
 import subprocess
 
 
-os.system('cls')
+#os.system('cls')
 
 
 load_dotenv()
@@ -140,7 +140,7 @@ bot = commands.Bot(command_prefix='.', intents=intents, help_command=CustomHelpC
 async def on_ready():
     print(f'{bot.user} is now running!')
     
-    await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name="Fate"))
+    await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.listening, name="Erika"))
     
     
     bot.loop.create_task(send_timed_message())
