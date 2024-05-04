eaf7480b1399cfb8ba33324d79ed0ece825804a5
Bernso <ben.w.sheridan@gmail.com>
2024-05-04 08:57:29 +0100

    yes

--- fakehack.py
+++ fakehack.py
@@ -1,17 +1,1 @@
-import random
-
-
-
-
-print("You have entered the danger zone.")
-
-for x in range(1,random.randint(1,80)):
-    print(f"Corrupted Files: {x}")
-print("\nYou computer has been compromised.")
-
-
-
-
-
-
-
+print("Beans")
